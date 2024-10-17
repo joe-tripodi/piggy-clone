7a434b81e5ed880000080b34b1b5c7c305aa93f1
joe-tripodi <joe@flowofcode.com>
2024-10-17 16:13:09 +0930

    add text to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
 export default function Home() {
   return (
-    <div></div>
+    <div
+      className="justify-center place-items-center text-center mx-auto lg:w-4/8 xl:w-3/4 z-10"
+    >
+      <h2
+        className="text-5xl text-center font-bold"
+      >
+        Grow your business with loyalty
+      </h2>
+      <p
+        className="text-xl mx-auto text-center text-gray-500"
+      >
+        Loyalty and engagement software to encourage behaviors in customers and
+        employees alike. Drive behaviors and communicate effortlessly from your
+        all-in-one dashboard.
+      </p>
+    </div>
   );
 }
