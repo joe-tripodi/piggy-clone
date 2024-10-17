b41c9ea58533fb544edbd7d11182028b172c56ae
joe-tripodi <joe@flowofcode.com>
2024-10-17 16:06:01 +0930

    correct footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -2,27 +2,21 @@ import Link from "next/link"
 
 export default function Footer() {
   return (
-    <div className="min-h-screen font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-12 flex-wrap items-center justify-center">
-        {
-          ["Legal", "Privacy", "Cookies", "Legal Notice"].map((item) => {
-            return (
-              <Link
-                className="transition-all ease-out-in duration-100 hover:underline hover:highlight"
-                href="#"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {item}
-              </Link>
-            )
-          })
-        }
-      </footer>
-    </div>
+    <footer className="bg-[#f3f2fa] row-start-3 flex gap-12 flex-wrap items-center justify-center">
+      {
+        ["Legal", "Privacy", "Cookies", "Legal Notice"].map((item) => {
+          return (
+            <Link
+              className="transition-all ease-out-in duration-100 hover:underline hover:highlight"
+              href="#"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {item}
+            </Link>
+          )
+        })
+      }
+    </footer>
   );
 }
